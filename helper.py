71d58de39b5d632157205a6687b57b6e55516a6c
marty-m <m.miliauskas2@ncl.ac.uk>
2024-02-15 00:41:21 +0000

    Use Pushover API and JSON files for storing bookings/cookies

--- helper.py
+++ helper.py
@@ -1,22 +1,66 @@
+import datetime
 import os
 from dotenv import load_dotenv
-from twilio.rest import Client
-
+from chump import Application
+import json
+import booking
 
 
 load_dotenv()
 
+PUSHOVER_APP_TOKEN= os.getenv("PUSHOVER_APP_TOKEN")
+PUSHOVER_USER_TOKEN = os.getenv("PUSHOVER_USER_TOKEN")
 
-TWILIO_ACCOUNT_SID = os.getenv("TWILIO_ACCOUNT_SID")
+def send_message(title, message, priority=0):
+    app = Application(PUSHOVER_APP_TOKEN)
+    user = app.get_user(PUSHOVER_USER_TOKEN)
+    user.send_message(title=title, message=message, priority=priority)
+
+def send_court_alert():
+    title = "New Court Booking(s) Available"
+    message = "New bookings available:\n"
+
+    availableBookings = get_stored_bookings()
+    availableBookingsCopy = availableBookings.copy()
+
+    for booking in availableBookings:
+        if booking.dateTime < datetime.datetime.now():
+            availableBookingsCopy.remove(booking)
+        elif booking.dateTime > datetime.datetime.now() and not booking.alerted:
+            message += f"Court {booking.courtNr} at {booking.dateTime.strftime('%d/%m/%Y %H:%M')}\n"
+            booking.alerted = True
+            availableBookingsCopy.append(booking)
+        
+    commit_booking_list(availableBookingsCopy)       
+    if message != "New bookings available:\n":
+        send_message(title, message)
+
+def get_stored_cookie():
+    if os.path.exists('cookie.json'):
+        with open('cookie.json', 'r') as file:
+            cookie = json.load(file)
+    return cookie
+
+def dump_cookie(cookie):
+    with open('cookie.json', 'w') as file:
+        json.dump(cookie, file)
 
 
-TWILIO_AUTH_TOKEN = os.getenv("TWILIO_AUTH_TOKEN")
+def get_stored_bookings():
+    if os.path.exists('bookings.json'):
+        with open('bookings.json', 'r') as file:
+            bookings = json.load(file)
+    return bookings
 
-twilioClient = Client(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN)
+def store_booking(booking: booking.Booking):
+    availableBookings = get_stored_bookings()
+    for i in availableBookings:
+        if i.dateTime == booking.dateTime and i.courtNr == booking.courtNr:
+            return
+    availableBookings.append(booking)
+    with open('bookings.json', 'w') as file:
+        json.dump(availableBookings, file)
 
-
-def send_message(destinationNr, message):
-    twilioClient.api.account.messages.create(
-                    to=destinationNr,
-                    from_="+447700142025",
-                    body=message)
+def commit_booking_list(bookingList):
+    with open('bookings.json', 'w') as file:
+        json.dump(bookingList, file)
