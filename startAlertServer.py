84858c3054e7b874756cac67dde788c6640ce3e3
marty-m <m.miliauskas2@ncl.ac.uk>
2024-03-17 13:15:00 +0000

    Add logging to startAlertServer.py

--- startAlertServer.py
+++ startAlertServer.py
@@ -3,6 +3,7 @@ import booking
 import datetime
 import court5
 import court8
+import logging
 from helper import store_booking, send_court_alert
 
 
@@ -10,19 +11,27 @@ from helper import store_booking, send_court_alert
 
 # TODO:
 # - Remote service pause and resume
+# - Store each user's booking list in separate files/db tables
+    # + modify send_court_alert to send alerts to each user based on their booking list"
 def main():
-    store_booking(booking.Booking(0, datetime.datetime(2021, 10, 10, 10, 10)))
-    while True:
-        print(f"---------{datetime.datetime.now().strftime('%d/%m/%Y   %H:%M:%S')}---------")
-        print("Starting alert service...")
-        print("Searching available slots for Court 5...")
-        court5.court5()
-        print("Searching available slots for Court 8...")
-        court8.court8()
-        print("Sending court alert... (if any new bookings are available)")
-        send_court_alert()
-        print("Waiting for 2 hours...")
-        time.sleep(7200)
+    while True:    
+        logging.basicConfig(filename="scraper.log", format='%(asctime)s %(message)s', filemode='w')
+        logger = logging.getLogger()
+        logger.setLevel(logging.INFO)
+        
+        store_booking(booking.Booking(0, datetime.datetime(2021, 10, 10, 10, 10)))
 
+
+        logger.info(f"---------{datetime.datetime.now().strftime('%d/%m/%Y   %H:%M:%S')}---------")
+        logger.info("Starting alert service...")
+        logger.info("Searching available slots for Court 5...")
+        court5.court5()
+        logger.info("Searching available slots for Court 8...")
+        court8.court8()
+        logger.info("Sending court alert... (if any new bookings are available)")
+        send_court_alert()
+        logger.info("Waiting for 1 hour...")
+        time.sleep(3600)
+        
 if __name__ == "__main__":
     main()
